59fcca512bfab2a0c81dd01a9daa48daa181cde2
Jesuschirre <154385363+jesuschirre@users.noreply.github.com>
2025-11-21 15:11:29 -0500

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,49 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Registro from './pages/Registro';
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Registro from "./pages/Registro";
 import FormNueVendedor from "./pages/FormNueVendedor";
+
 import DashboardLayout from "./pages/DashboardLayout";
 import DashboardHome from "./pages/dashboard/DashboardHome";
 import Perfil from "./pages/dashboard/Perfil";
 import Tickets from "./pages/dashboard/Tickets";
+
+import ProtectedRoute from "./ProtectedRoute";
+
 function App() {
   return (
     <AuthProvider>
-      <Routes >
+      <Routes>
+        {/* Rutas públicas */}
         <Route path="/" element={<Home />} />
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/registro' element={<Registro/>}/>
-        <Route path="/FormNvend" element={<FormNueVendedor/>}/>
+        <Route path="/login" element={<Login />} />
+        <Route path="/registro" element={<Registro />} />
 
-        {/* Ruta padre para el dashboard */}
-        <Route path="/dashboard" element={<DashboardLayout />}>
-            {/* Aquí van las rutas hijas del dashboard */}
-          <Route index element={<DashboardHome />} />  {/* /dashboard */}
-            {/* ruta perfil */}
-          <Route path="Perfil" element={<Perfil/>}/>
-            {/* ruta tikets */}
-          <Route path="Tickets" element={<Tickets/>}/>
+        {/* Rutas protegidas */}
+        <Route
+          path="/FormNvend"
+          element={
+            <ProtectedRoute>
+              <FormNueVendedor />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <DashboardLayout />
+            </ProtectedRoute>
+          }
+        >
+          <Route index element={<DashboardHome />} />
+          <Route path="Perfil" element={<Perfil />} />
+          <Route path="Tickets" element={<Tickets />} />
         </Route>
-
       </Routes>
     </AuthProvider>
   );
